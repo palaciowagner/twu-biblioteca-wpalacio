69b2ccbdba047053f45a85f97583b979fc00263c
Wagner Palacio <palaciowagner@gmail.com>
2018-01-11 13:40:58 -0300

    1 - Welcome Message

---
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+public class Biblioteca {
+
+    public Biblioteca(){
+        showWelComeMessage();
+    }
+
+    private static void showWelComeMessage() {
+         System.out.print("===Welcome to TWU Biblioteca===");
+    }
+}
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,30 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void shouldShowWelcomeMessage(){
+        Biblioteca biblioteca = new Biblioteca();
+        assertEquals(outContent.toString(), "===Welcome to TWU Biblioteca===");
+
     }
 }

4dcc5daf75645a8db47c02a23b77e58b36470a2f
Wagner Palacio <palaciowagner@gmail.com>
2018-01-14 21:02:48 -0300

    Creating User

---
+++ src/com/twu/biblioteca/entities/User.java
@@ -0,0 +1,28 @@
+package com.twu.biblioteca.entities;
+
+public class User {
+
+    private String libraryNumber;
+    private String password;
+
+    public User(String libraryNumber, String password){
+        this.libraryNumber = libraryNumber;
+        this.password = password;
+    }
+
+    public String getLibraryNumber() {
+        return libraryNumber;
+    }
+
+    public void setLibraryNumber(String libraryNumber) {
+        this.libraryNumber = libraryNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
